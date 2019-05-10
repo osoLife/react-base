ed2d093fc514dad8aef7c72f743d0af27f8063cc
osoLife <813188657@qq.com>
2019-05-10 08:41:28 +0800

    增加样式处理

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,23 @@ module.exports = {
         rules: [
             {
                 test: /\.js|jsx$/, use: 'babel-loader', exclude: /node_modules/
-
+            },
+            {
+                test: /\.css$/, use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader?modules&localIdentName=[path][name]-[local]-[hash:5]', 'sass-loader']
+            },
+            {
+                test: /\.ttf|woff|woff2|eot|svg$/, use: ['url-loader']
             }
         ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json'],
+        alias: {
+            '@': path.join(__dirname, './src')
+        }
     }
 };
